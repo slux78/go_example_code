66bd70995ab8d76f48441133a6730f66ad263f4d
manime <slux78@gmail.com>
2016-02-18 03:29:54 +0900

    add hello file and test reverse file for string pkg

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nf/string"
+)
+
+func main() {
+	fmt.Println("Hello, new gopher!")
+	fmt.Println(string.Reverse("Hell, new gopher!"))
+	fmt.Println("Hello world")
+}
